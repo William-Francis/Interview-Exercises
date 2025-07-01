e011336b943a13fb1cccd1454efb5f9da60432a3
William Francis <William-Francis@users.noreply.github.com>
2025-07-01 19:26:03 +0200

    test: expanded unit tests (All passing)

--- td-fullstack-assignment-master/src/utils.test.js
+++ td-fullstack-assignment-master/src/utils.test.js
@@ -20,8 +20,63 @@ describe('Detect sums', () => {
 
   it('should detect sums in order', () => {
     const result = detectSums([1, 2, 3]);
-    expect(result).to.be.instanceof(Array);
-    expect(result).to.have.lengthOf(1);
-    expect(result).to.deep.include({ pA: 0, pB: 1, sum: 2});
+    expect(result).to.deep.equal([{ pA: 0, pB: 1, sum: 2}]);
+  });
+
+  it('should detect multiple sums in [1, 2, 3, 4]', () => {
+    const result = detectSums([1, 2, 3, 4]);
+    expect(result).to.deep.equal([
+      { pA: 0, pB: 1, sum: 2 },
+      { pA: 0, pB: 2, sum: 3 }
+    ]);
+  });
+
+  it('should handle palindromes correctly and zeros [3, 0, 3]', () => {
+    const result = detectSums([3, 0, 3]);
+    expect(result).to.deep.equal([
+      { pA: 0, pB: 1, sum: 2 },
+      { pA: 1, pB: 2, sum: 0 }
+    ]);
+  });
+
+  it('should not use same element twice [1, 2, 4]', () => {
+    const result = detectSums([1, 2, 4]);
+    expect(result).to.deep.equal([]);
+  });
+
+  it('should not use same element twice [3, 0, 2]', () => {
+    const result = detectSums([3, 0, 2]);
+    expect(result).to.deep.equal([]);
+  });
+
+  it('should detect multiple combinations in [1, 2, 3, 4, 5]', () => {
+    const result = detectSums([1, 2, 3, 4, 5]);
+    expect(result).to.deep.equal([
+      { pA: 0, pB: 1, sum: 2 },
+      { pA: 0, pB: 2, sum: 3 },
+      { pA: 0, pB: 3, sum: 4 },
+      { pA: 1, pB: 2, sum: 4 }
+    ]);
+  });
+
+  it('should return every possible combination once [1, 2, 1, 3]', () => {
+    const result = detectSums([1, 2, 1, 3]);
+    expect(result).to.deep.equal([
+      { pA: 0, pB: 1, sum: 3 },
+      { pA: 0, pB: 2, sum: 1 },
+      { pA: 1, pB: 2, sum: 3 }
+    ]);
+  });
+
+  it('should return every possible combination once [1, 2, 1, 2, 3]', () => {
+    const result = detectSums([1, 2, 1, 2, 3]);
+    expect(result).to.deep.equal([
+      { pA: 0, pB: 1, sum: 4 },
+      { pA: 0, pB: 2, sum: 1 },
+      { pA: 0, pB: 2, sum: 3 },
+      { pA: 0, pB: 3, sum: 4 },
+      { pA: 1, pB: 2, sum: 4 },
+      { pA: 2, pB: 3, sum: 4 }
+    ]);
   });
 });

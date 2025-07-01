a37b79897ed7f4850dffa98aef60911e50581122
William Francis <William-Francis@users.noreply.github.com>
2025-07-01 18:46:30 +0200

    feature Implemented basic solution for detectSums
    ## Implemented detectSums which finds corresponding sums of inputs and their answer inside an array.
    ## Also added .prettierrc to keep existing project format

--- td-fullstack-assignment-master/src/utils.js
+++ td-fullstack-assignment-master/src/utils.js
@@ -1,5 +1,24 @@
 export const detectSums = (array) => {
-  return null;
+  let arrayFormatted = array.split(',').map(i => parseInt(i.trim(), 10));
+  let resultArray = [];
+  for (let i = 0; i < arrayFormatted.length; i++) {
+    for (let j = i + 1; j < arrayFormatted.length; j++) {
+      if (arrayFormatted.includes(arrayFormatted[i] + arrayFormatted[j])) {
+        const sumIndex = arrayFormatted.reduce(function (arr, element, index) {
+          if (element === arrayFormatted[i] + arrayFormatted[j] && index !== i && index !== j) arr.push(index);
+          return arr;
+        }, []);
+        for (let k = 0; k < sumIndex.length; k++) {
+          resultArray.push({
+            pA: i,
+            pB: j,
+            sum: sumIndex[k],
+          });
+        }
+      }
+    }
+  }
+  return '[' + resultArray.map((item) => JSON.stringify(item)).join(',') + ']';
 };
 
 export function calculateResult(input) {
@@ -11,5 +30,5 @@ export function calculateResult(input) {
   } catch (e) {
     error = e.message;
   }
-  return { input: parsedInput, result, error }
+  return { input: parsedInput, result, error };
 }
